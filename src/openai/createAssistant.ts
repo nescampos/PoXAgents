79980761ce3549de23caf03e114119acd6366b2a
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2025-03-13 08:55:10 -0300

    Add more parameters

--- src/openai/createAssistant.ts
+++ src/openai/createAssistant.ts
@@ -10,11 +10,28 @@ import { assistantPrompt } from "../constants/prompt";
  */
 export async function createAssistant(client: OpenAI): Promise<Assistant> {
   // Create the assistant with the specified model, name, instructions and tools.
+
+  if (!process.env.OPENAI_API_KEY) {
+      throw new Error(
+      "OPENAI_API_KEY environment variable is not set. You need to set it to create the assistant."
+      );
+  }
+  if (!process.env.OPENAI_ASSISTANT_NAME) {
+      throw new Error(
+      "OPENAI_ASSISTANT_NAME environment variable is not set. You need to set it to create the assistant."
+      );
+  }
+  if (!process.env.OPENAI_MODEL) {
+      throw new Error(
+      "OPENAI_MODEL environment variable is not set. You need to set it to create the assistant."
+      );
+  }
+
   return await client.beta.assistants.create({
     // The model to use for the assistant.
-    model: "gpt-4o-mini",
+    model: process.env.OPENAI_MODEL,
     // The name of the assistant.
-    name: "Darth Vader",
+    name: process.env.OPENAI_ASSISTANT_NAME,
     // The instructions to provide to the assistant.
     instructions: assistantPrompt,
     // The tools to provide to the assistant.
